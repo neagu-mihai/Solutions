1da7830b4bf17d779225be30500e07bfdf5b0dc1
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2022-03-26 17:00:32 +0200

    no target

--- tp1/target/debug/deps/tp1-89816d775f06188f.d
+++
@@ -1,6 +0,0 @@
-/home/alexandru/projects/sde2/Solutions/tp1/target/debug/deps/tp1-89816d775f06188f.rmeta: src/main.rs src/tests.rs
-
-/home/alexandru/projects/sde2/Solutions/tp1/target/debug/deps/tp1-89816d775f06188f.d: src/main.rs src/tests.rs
-
-src/main.rs:
-src/tests.rs:
--- tp1/target/debug/deps/tp1-d88b66d9f6455651.d
+++
@@ -1,5 +0,0 @@
-/home/alexandru/projects/sde2/Solutions/tp1/target/debug/deps/tp1-d88b66d9f6455651.rmeta: src/main.rs
-
-/home/alexandru/projects/sde2/Solutions/tp1/target/debug/deps/tp1-d88b66d9f6455651.d: src/main.rs
-
-src/main.rs:
