6d99eac38b551a407b841785e762daada6546994
unduhhh <mihai_neagu_09@yahoo.com>
2022-05-10 11:46:59 +0300

    Update main.rs

--- tp9/ex2/src/main.rs
+++ tp9/ex2/src/main.rs
@@ -5,14 +5,23 @@ use std::net::TcpStream;
 fn main() {
     // TODO 1 - use the arguments to get an address and a port
     // cargo run address port
-    let address = "".to_string();
-    let port = 0;
+    let args: Vec<String> = env::args().collect();
+    let address = args[1].clone().to_string();
+    let port =args[2].clone().to_string();
+
     match TcpStream::connect(format!("{}:{}", address, port)) {
         Ok(mut stream) => {
             println!("Connected to {}", stream.peer_addr().unwrap());
             stream.write("Hello".as_bytes()).unwrap();
 
             // TODO 2 read back from the server
+            println!("Client connection {}", stream.peer_addr().unwrap());
+            let mut buffer = [0; 1024]; 
+           let bytes = stream.read(&mut buffer).unwrap();
+                    println!("read {} bytes", bytes);
+                    let s = String::from_utf8_lossy(&buffer[0..bytes]);
+
+                    println!("Message: {}", s);
         }
         Err(error) => {
             println!("Connection failed: {}", error);
